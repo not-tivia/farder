f1e623863f355b5d966bfa417a3a4633b94ea81c
not-tivia <not-tivia@users.noreply.github.com>
2026-05-04 00:15:39 -0700

    refactor(client): move xp-theme.css into themes/xp-luna-blue/ with metadata

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/xp-theme.css";
+import "./themes/xp-luna-blue/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
